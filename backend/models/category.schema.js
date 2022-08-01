24f7ded4c38bf1d9cf9d47433b3e592e934b259b
George Maina <george.maina@finova.tech>
2022-08-01 15:08:43 +0200

    added category schema

---
+++ backend/models/category.schema.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+});
+
+
+//export a model created from the schema
+exports.Category = mongoose.model("Category", categorySchema);
